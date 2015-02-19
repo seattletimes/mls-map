d3685f41ba310129d09ed13315725fc72b0754d4
Audrey Carlsen <acarlsen@seattletimes.com>
2015-02-18 16:51:56 -0800

    hello map

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 //Use CommonJS style via browserify to load other modules
+
+var L = require("leaflet");
+
+var map = L.map('map').setView([25, 0], 2);
+
+L.tileLayer('//{s}.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+  subdomains: ["server", "services"],
+  attribution: "Esri, NAVTEQ, DeLorme" 
+}).addTo(map);
