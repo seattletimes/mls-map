580f3cfaa8fa796eca198668faa033229d7b3e64
Thomas Wilburn <news@thomaswilburn.net>
2015-03-04 16:33:22 -0800

    Cleanup to prep for legend changes

--- src/js/main.js
+++ src/js/main.js
@@ -10,16 +10,34 @@ var legendHTML = require("./_legend.html");
 var team = $(".mobile-menu select").val() || "mls";
 var worldLayer;
 
+var legend = $(".legend");
+var soundersTime = $(".toggle button");
+
 ich.addTemplate("popup", popupHTML);
 ich.addTemplate("legend", legendHTML);
 
-var map = L.map('map');
-map.fitBounds([
-    [-28, -135],
-    [63, 14],
-    [-40, 125],
-    [-41, -68]
-]);
+var map = L.map('map', {
+  scrollWheelZoom: false
+});
+
+var bounds = [
+  [-28, -135],
+  [63, 14],
+  [-40, 125],
+  [-41, -68]
+];
+
+map.fitBounds(bounds);
+$(window).on("resize", function() { map.fitBounds(bounds) });
+
+var restyle = function(feature) {
+  return { 
+    color: "black",
+    fillColor: fillColor(team, feature.properties.name),
+    weight: 1,
+    fillOpacity: 1
+  };
+};
 
 var request = $.ajax({
   url: "assets/countries.geo.json",
@@ -73,16 +91,7 @@ var openTooltip = function(event){
   }
 };
 
-var restyle = function(feature) {
-  return { 
-    color: "black",
-    fillColor: fillColor(team, feature.properties.name),
-    weight: 1,
-    fillOpacity: 1
-  };
-};
-
-$(".toggle button").click(function(e){
+soundersTime.click(function(e){
   team = $(e.target).data("team");
   worldLayer.setStyle(restyle);
   map.closePopup();
@@ -90,16 +99,38 @@ $(".toggle button").click(function(e){
   $(e.target).addClass("selected");
 });
 
+// first color is lightest, last is darkest
+var colors = {
+  "red": [ "#e1a8a9", "#e16667", "#e32527", "#ab1b1d", "#831213" ],
+  "green": [ "#bedbb0", "#80b069", "#5d9741", "#3d7a20", "#23580a" ],
+  "yellow": [ "#ffec9f", "#ffe681", "#ffdb4b", "#c5a524", "#987900" ],
+  "orange": [ "#f4d3c9", "#f4af9a", "#f28260", "#f46b40", "#f44d19" ],
+  "gray": [ "#bbb", "#999", "#777", "#555", "#333" ],
+  "purple": [ "#d3c5e1", "#936fb8", "#7346a1", "#491d76", "#3c0f69" ]
+};
+
+//build team min-max ranges
+
 var changeTeam = function(t) {
-  team = t;
-  worldLayer.setStyle(restyle);
+  team = t || team;
+  if (worldLayer) worldLayer.setStyle(restyle);
   map.closePopup();
   if (team == "seattle") {
-    $(".toggle button").show();
+    soundersTime.removeClass("selected").show().filter("[data-team=\"seattle\"]").addClass("selected");
   } else {
-    $(".toggle button").hide();
+    soundersTime.hide();
   }
-}
+
+  var data = team == "mls" ? worldData : teamData[team];
+
+  legend.html(ich.legend({
+    name: data.team || "MLS",
+    img: team,
+    colors: team == "mls" ? colors.red : colors.green
+  }));
+};
+
+changeTeam();
 
 $(".badge img").click(function(e){
   changeTeam(e.target.getAttribute("data-team"));
@@ -113,12 +144,6 @@ var fillColor = function(teamId, country) {
   var source;
   if (teamId == "mls") {
     // All teams, 2015
-
-    $(".legend").html(ich.legend({
-      teamLabel: "MLS", 
-      img: "mls", 
-      colors: colors.red
-    })); 
 
     if (worldData[country]) {
       var players = parseInt(worldData[country].players);
@@ -139,12 +164,6 @@ var fillColor = function(teamId, country) {
   } else if (teamId == "sounders") {
     // Sounders, 2009-2015
 
-    $(".legend").html(ich.legend({
-      teamLabel: "Seattle Sounders", 
-      img: "seattle", 
-      colors: colors.green
-    })); 
-
     if (soundersData[country]) {
       var players = parseInt(soundersData[country].players);
       if (players > 11) {
@@ -162,16 +181,10 @@ var fillColor = function(teamId, country) {
       return "white";
     }
   } else {
-    if (teamData[teamId][country]) {
+    if (teamData[teamId].countries[country]) {
       // Specific team, 2015
-      var players = parseInt(teamData[teamId][country].players);
+      var players = parseInt(teamData[teamId].countries[country].players);
       var color = colors[teamData[teamId].color];
-
-      $(".legend").html(ich.legend({
-        teamLabel: teamData[teamId].team, 
-        img: teamId, 
-        colors: color
-      }));
 
       if (players > 11) {
         return color[4];
@@ -188,14 +201,4 @@ var fillColor = function(teamId, country) {
       return "white";
     }
   }
-};
-
-// first color is lightest, last is darkest
-var colors = {
-  "red": [ "#e1a8a9", "#e16667", "#e32527", "#ab1b1d", "#831213" ],
-  "green": [ "#bedbb0", "#80b069", "#5d9741", "#3d7a20", "#23580a" ],
-  "yellow": [ "#ffec9f", "#ffe681", "#ffdb4b", "#c5a524", "#987900" ],
-  "orange": [ "#f4d3c9", "#f4af9a", "#f28260", "#f46b40", "#f44d19" ],
-  "gray": [ "#bbb", "#999", "#777", "#555", "#333" ],
-  "purple": [ "#d3c5e1", "#936fb8", "#7346a1", "#491d76", "#3c0f69" ]
 };
